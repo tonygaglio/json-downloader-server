2f3670fecd5f7d6b44f7a6a6f3bb7aa476ca0e1c
tonygaglio <48930658+tonygaglio@users.noreply.github.com>
2023-07-26 14:53:56 +0000

    add multi-origin support

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const port = 9000;
 const routes = require('./routes/v1');
 const cors = require('cors');
 
+const allowedOrigins = ['https://downloader.anthony.media', 'http://localhost:3000'];
+
 app.use(
-  cors({
-    origin: 'https://downloader.anthony.media',
-  })
+    cors({
+        origin: (origin, callback) => {
+            // Check if the request origin is allowed
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+    })
 );
 
 // v1 api routes
@@ -15,5 +24,5 @@ app.use('/v1', routes);
 app.use('/', (req, res) => res.send({ online: true }));
 
 app.listen(port, () => {
-  console.log(`JSONDownloader Server listening on port ${port}`);
+    console.log(`JSONDownloader Server listening on port ${port}`);
 });
